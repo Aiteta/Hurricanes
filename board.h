d13905a827cd2a83118ec698eaddf2e528d6fa1b
Alexandre Ait Ettajer <alexandre.aitettajer@gmail.com>
2021-06-08 23:38:37 -0500

    Started Board Class

---
+++ board.h
@@ -0,0 +1,12 @@
+class Board {
+  public:
+
+  private:
+    std::vector<Zone> allZones;
+    std::vector<Weather> drawWeather;
+    std::vector<Weather> discardWeather;
+    std::vector<Hurricane> activeHurricanes;
+    std::vector<Hurricane> inactiHurricanes;
+    std::vector<Players> allPlayers;
+}
+
